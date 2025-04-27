d622dff40e0c772ef8a12c3138fe1647ec182d51
MarceloPortillo <marcelo160891@gmail.com>
2025-04-27 20:11:40 -0300

    Se agrega los modelos de Categoria y Servicio Habitacion

--- django-viajeFacil/hotel/models.py
+++ django-viajeFacil/hotel/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+class Servicio(models.Model):
+    nombre_servicio = models.CharField(max_length=100)
+    descripcion_servicio = models.TextField(max_length=500)
+
+class Meta:
+    ordering = ['-nombre_servicio']
+
+def _str_(self):
+    return self.nombre_servicio
+
+class Categoria(models.Model):
+    nombre_categoria = models.CharField(max_length=100)
+    descripcion_categoria = models.TextField(max_length=500, blank=True)
+    capacidad_huesped = models.IntegerField()
+    cantidad_hab = models.IntegerField()
+    imagen_categoria = models.ImageField(upload_to='habitaciones/%Y/%m/%d',
+                                        blank=True)
+    precio_base_categoria = models.DecimalField(max_digits=20, decimal_places=2)
+    estado_categoria = models.BooleanField(default=True)
+
+class Meta:
+        ordering = ['nombre_categoria']
+
+def _str_(self):
+    return self.nombre_categoria

f7fd24810528fd4b4f91ce9db7e150322968eb8c
MarceloPortillo <marcelo160891@gmail.com>
2025-04-29 21:28:15 -0300

    Se deja funcionando la lista de hoteles recuperados de la base de datos

--- django-viajeFacil/viajefacil/hotel/views.py
+++ django-viajeFacil/viajefacil/hotel/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
+from .models import Hotel
+from .models import Categoria
 
 def index_alojamientos (request):
     return render (request, 'index_alojamientos.html')
 
 def lista_hoteles (request):
     return render (request, 'lista_hoteles.html')
+
+def lista_hoteles(request):
+    hoteles = Hotel.objects.all().prefetch_related('habitaciones__categoria')
+    return render(request, 'lista_hoteles.html', {'hoteles': hoteles})
+
